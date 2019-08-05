154513df4e22bcbf80c099bf7ddfe845167e1d99
Gerardo Manzo <programmingmanzo@gmail.com>
2019-08-05 02:10:33 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -103,6 +103,17 @@ class Student
 
     DB[:conn].execute(sql)
   end
+  
+    def self.all_students_in_grade_x(grade)
+    sql = <<-SQL
+      SELECT * FROM students
+      WHERE grade = ?
+      SQL
+
+      DB[:conn].execute(sql, grade).map do |row|
+        self.new_from_db(row)
+      end
+  end
 
   def self.drop_table
     sql = "DROP TABLE IF EXISTS students"
